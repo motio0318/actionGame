01027433e49b0556e3efb17bf0434f9ccad00a99
DESKTOP-U0TU0SG\GA2B <h1202315008@hiratagakuen.onmicrosoft.com>
2024-10-10 15:19:32 +0900

    InputSystem追加　移動ジャンプ実装

---
+++ ActionGame/Assets/Script/Enemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField, Header("ˆÚ“®‘¬“x")]
+    private float moveSpeed;
+
+    private Rigidbody2D rigid;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+    }
+
+
+    private void Move()
+    {
+        rigid.velocity = new Vector2(Vector2.left.x * moveSpeed, rigid.velocity.y);
+    }
+}
--- ActionGame/Assets/Script/PlayerManagement.cs
+++ ActionGame/Assets/Script/PlayerManagement.cs
@@ -1,92 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
 public class PlayerManagement : MonoBehaviour
 {
-    public LayerMask StageLayer;
-    // 動作状態を定義する
-    private enum MOVE_TYPE
-    {
-        STOP,
-        RIGHT,
-        LEFT,
-    }
-    MOVE_TYPE move = MOVE_TYPE.STOP; // 初期状態はSTOP
-    Rigidbody2D rbody2D;             // Rigidbody2Dを定義
-    float speed;                     // 移動速度を格納する変数
-    private void Start()
-    {
-        // Rigidbody2Dのコンポーネントを取得
-        rbody2D = GetComponent<Rigidbody2D>();
-    }
-    private void Update()
-    {
-        float horizonkey = Input.GetAxis("Horizontal");      // 水平方向のキー取得
-                                                             // 取得した水平方向のキーを元に分岐
-        if (horizonkey == 0)
-        {
-            // キー入力なしの場合は停止
-            move = MOVE_TYPE.STOP;
-        }
-        else if (horizonkey > 0)
-        {
-            // キー入力が正の場合は右へ移動する
-            move = MOVE_TYPE.RIGHT;
-        }
-        else if (horizonkey < 0)
-        {
-            // キー入力が負の場合は左へ移動する
-            move = MOVE_TYPE.LEFT;
-        }
-        // spaceを押したらジャンプ関数へ
-        if (GroundChk())
-        {
-            if (Input.GetKeyDown("space"))
-            {
-                Jump();
-            }
-        }
-    }
-    // 物理演算(rigidbody)はFixedUpdateで処理する
-    private void FixedUpdate()
-    {
-        //回転のターゲット角度を決める
-        float targetRotation = 0f;
-        Vector3 scale = transform.localScale;
-        if (move == MOVE_TYPE.STOP)
-        {
-            speed = 0;
-        }
-        else if (move == MOVE_TYPE.RIGHT)
-        {
-            targetRotation = 0f; // 右向き
-            speed = 3;
-        }
-        else if (move == MOVE_TYPE.LEFT)
-        {
-            targetRotation = 180f; // 左向き
-            speed = -3;
-        }
+    [SerializeField, Header("移動速度")]
+    private float moveSpeed;
+    [SerializeField, Header("ジャンプ速度")]
+    private float jumpSpeed;
 
-        //プレイヤーの回転を滑らかに目標角度へ補間
-        Quaternion targetQuaternion = Quaternion.Euler(0, targetRotation, 0);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetQuaternion, Time.deltaTime * 10f);//数字は補間速度の調整
 
-        //rigidbody2Dのvelocity(速度)へ取得したspeedを入れる。y方向は動かないのでそのままにする
-        rbody2D.velocity = new Vector2(speed, rbody2D.velocity.y);
-    }
-    void Jump()
+    private Vector2 inputDirection;
+    private Rigidbody2D rigid;
+    private bool bJump;
+
+     void Start()
     {
-        // 上に力を加える
-        rbody2D.AddForce(Vector2.up * 300);
+        rigid = GetComponent<Rigidbody2D>();
+        bJump = false;
     }
-    bool GroundChk()
+
+     void Update()
     {
-        Vector3 startposition = transform.position;                     // Playerの中心を始点とする
-        Vector3 endposition = transform.position - transform.up * 1.40f; // Playerの足元を終点とする
-                                                                        // Debug用に始点と終点を表示する
-        Debug.DrawLine(startposition, endposition, Color.red);
-        // Physics2D.Linecastを使い、ベクトルとStageLayerが接触していたらTrueを返す
-        return Physics2D.Linecast(startposition, endposition, StageLayer);
+
+        Move();
+
     }
+
+    private void Move()
+    {
+        rigid.velocity = new Vector2(inputDirection.x * moveSpeed, rigid.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Tilemap")
+        {
+            bJump = false;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputDirection = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed || bJump) return;
+
+        rigid.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+        bJump = true;
+
+    }
+
+
 }
+
