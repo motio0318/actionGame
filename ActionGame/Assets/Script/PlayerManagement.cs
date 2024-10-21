ce0845b812bb92defc45e566e513c8c8be650fbd
DESKTOP-CB6AQV3\GA2B <h1202315008@hiratagakuen.onmicrosoft.com>
2024-10-21 16:15:28 +0900

    無敵時間実装

--- ActionGame/Assets/Script/PlayerManagement.cs
+++ ActionGame/Assets/Script/PlayerManagement.cs
@@ -8,52 +8,80 @@ public class PlayerManagement : MonoBehaviour
 {
     [SerializeField, Header("移動速度")]
     private float moveSpeed;
+
     [SerializeField, Header("ジャンプ速度")]
     private float jumpSpeed;
+
     [SerializeField, Header("HP")]
     private int hp = 3;
+
+    [SerializeField, Header("無敵時間")]
+    private float damageaTime;
+
+    [SerializeField, Header("点滅時間")]
+    private float flashTime;
+
 
 
 
     private Vector2 inputDirection;
     private Rigidbody2D rigid;
     private Animator anime;
+    private SpriteRenderer spriteRenderer;
     private bool bJump;
 
      void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         bJump = false;
 
     }
 
-     void Update()
+     void FixedUpdate()
     {
 
         //LookMoveDirec();
         Move();
-
         LookMoveDirec();
 
     }
 
     private void Move()
     {
-        rigid.velocity = new Vector2(inputDirection.x * moveSpeed, rigid.velocity.y);
+        // 現在の位置を取得し、入力方向に基づいて新しい位置を計算
+        Vector3 newPosition = transform.position + new Vector3(inputDirection.x, 0, 0) * moveSpeed * Time.deltaTime;
+        // 計算した位置にオブジェクトを移動
+        transform.position = newPosition;
         anime.SetBool("Walk", inputDirection.x != 0.0f);
     }
 
     private void LookMoveDirec()
+
     {
+        Vector3 currentScale = transform.localScale;
+
         if (inputDirection.x > 0.0f)
+
         {
-            transform.eulerAngles = Vector3.zero;
+
+            // 右向きにする (スケールを正に設定)
+
+            transform.localScale = new Vector3(Mathf.Abs(currentScale.x), transform.localScale.y, transform.localScale.z);
+
         }
+
         else if (inputDirection.x < 0.0f)
+
         {
-            transform.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
+
+            // 左向きにする (スケールを反転)
+
+            transform.localScale = new Vector3(-Mathf.Abs(currentScale.x), transform.localScale.y, transform.localScale.z);
+
         }
+
     }
 
 
@@ -67,7 +95,8 @@ public class PlayerManagement : MonoBehaviour
         if(collision.gameObject.tag == "Enemy")
         {
            bool enemyDefeated =  HitEnemy(collision.gameObject,collision);
-
+            gameObject.layer = LayerMask.NameToLayer("PlayerDamage");
+            StartCoroutine(Damage());
             if(!enemyDefeated)
             {
                 //ダメージを受ける処理
@@ -94,6 +123,25 @@ public class PlayerManagement : MonoBehaviour
         return false;
 
     }
+
+
+    IEnumerator Damage()
+    {
+        Color color = spriteRenderer.color;
+        for(int i = 0;i < damageaTime; i++)
+        {
+            yield return new WaitForSeconds(flashTime);
+            spriteRenderer.color = new Color(color.r, color.g, color.b, 0.0f);
+
+            yield return new WaitForSeconds(flashTime);
+            spriteRenderer.color = new Color(color.r, color.g, color.b, 1.0f);
+
+        }
+        spriteRenderer.color = color;
+        gameObject.layer = LayerMask.NameToLayer("Default");
+
+    }
+
     private void TakeDamage(int damage)
     {
         hp -= damage;
